d811526ed62139b4131e963dceea526dbfea4c34
dani <space.dg23@gmail.com>
2019-03-11 14:52:18 +0100

    Components

--- vue/main.js
+++ vue/main.js
@@ -2,37 +2,41 @@
 Vue.component('usuarios', {
     template: '#usuarios-template',
     mounted() {
-        axios.get('https://randomuser.me/api/?results=100')
-        .then((datos) => {
-            console.log(datos)
-             const listado = datos.data.results.map((usuario) => {
-                return {
-                    nombre: `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`,                    
-                    email: usuario.email,
-                    foto: usuario.picture.large,
-                }
-               
-           });
-           this.usuarios = listado;
-        });
+        axios.get('https://randomuser.me/api/?results=100&nat=es')
+            .then((datos) => {
+                const listado = datos.data.results.map((usuario) => {
+                    return {
+                        nombre: `${usuario.name.first.charAt(0).toUpperCase() + usuario.name.first.slice(1)}
+                             ${usuario.name.last.charAt(0).toUpperCase() + usuario.name.last.slice(1)}`,
+                        genero: usuario.gender,
+                        email: usuario.email,
+                        foto: usuario.picture.large,
+                        localizacion: usuario.location.city.charAt(0).toUpperCase() + usuario.location.city.slice(1),
+                    }
+                });
+                this.usuarios = listado;
+                console.log(this.usuarios)
+            });
     },
-    data () {
+    data() {
         return {
             usuarios: [],
-            busqueda: '',
+            busquedaUsuario: '',
+            busquedaCiudad: '',
         }
     },
     computed: {
-        filtrarUsuarios() {
-             return this.usuarios.filter((usuarios) => {
-                  return usuarios.nombre.includes(this.busqueda);
-             });
-        }
-    }
-
+        filtrarUsuarios () {
+            return this.usuarios.filter((usuarios) => {
+                return usuarios.nombre.toLowerCase().includes(this.busquedaUsuario.toLowerCase())
+                && usuarios.localizacion.toLowerCase().includes(this.busquedaCiudad.toLowerCase()) 
+            });
+       },        
+    },
+    
 });
 //COMPONENT CHILD
-Vue.component('usuario', {
+Vue.component('usuario-target', {
     props: ['datos'],
     template: '#usuario-template',
 })
